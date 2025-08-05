40ea29b35d686d9f1d74afa749765c2b904dcfc1
Sharath91130 <sharathkurer8055@gmail.com>
2025-08-05 15:05:37 +0530

    program1 updates sucessfully

--- Program1.java
+++ Program1.java
@@ -1,5 +1,6 @@
 public class Program1 {
     public static void main(String[] args) {
-        System.out.println("Pgm 1");
+        System.out.println("Pgm 1  this code was change when i clone this i want make " +
+                "changes and see prevoius and this one" );
     }
 }
